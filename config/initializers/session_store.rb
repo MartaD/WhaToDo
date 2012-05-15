fe3e30a031e219307b35a8953524ae9defe0c1b8
Krzysiek Kutt <krzysztof.kutt@gmail.com>
2012-05-15 23:19:16 +0200

    pierwszy commit; dwa przypisania poprawione na zgodne z Ruby 1.8.7

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-WhaToDo::Application.config.session_store :cookie_store, key: '_WhaToDo_session'
+WhaToDo::Application.config.session_store :cookie_store, :key => '_WhaToDo_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
